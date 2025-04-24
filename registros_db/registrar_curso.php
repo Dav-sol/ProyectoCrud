0f51a5b500f79fa1bc3af1790b1555d0f81982e4
David Solano <david.solano15@hotmail.com>
2025-04-24 09:43:50 -0500

    Actualizacion automatica

--- registros_db/registrar_curso.php
+++ registros_db/registrar_curso.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar Curso</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" </head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
 
 <body>
     <section class="m-5">
